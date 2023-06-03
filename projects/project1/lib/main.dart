65440c011282bf0dd86b026ca0d460ada4f699cd
Thunder <mahmmoud.hassanein@gmail.com>
2023-06-03 15:14:24 +0300

    How to configure Widgets & Objects.

--- projects/project1/lib/main.dart
+++ projects/project1/lib/main.dart
@@ -6,11 +6,16 @@ void main() {
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-            Color.fromARGB(255, 143, 129, 165),
-          ])),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+                Color.fromARGB(255, 143, 129, 165),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
           child: const Center(
             child: Text("Hello World!"),
           ),
