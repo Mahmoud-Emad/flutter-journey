d37b783b747972be23f385ad1984af28d56c87db
Thunder <mahmmoud.hassanein@gmail.com>
2023-06-04 10:53:22 +0300

    Splitting code across files.

---
+++ projects/project1/lib/gradient_container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+            Color.fromARGB(255, 143, 129, 165),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World!",
+          style: TextStyle(
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- projects/project1/lib/main.dart
+++ projects/project1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project1/gradient_container.dart';
 
 void main() {
   runApp(
@@ -6,34 +7,4 @@ void main() {
       home: Scaffold(body: GradientContainer()),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-            Color.fromARGB(255, 143, 129, 165),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          "Hello World!",
-          style: TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
