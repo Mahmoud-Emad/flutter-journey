adbcfb23eba9a16229ba9755cb0705d6a35f1dca
Thunder <mahmmoud.hassanein@gmail.com>
2023-06-05 16:19:25 +0300

    Variables & types.

--- projects/project1/lib/gradient_container.dart
+++ projects/project1/lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:project1/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
