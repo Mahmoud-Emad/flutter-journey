bd926e182330c3dd023dbe247f43c091a728e98d
Thunder <mahmmoud.hassanein@gmail.com>
2023-06-07 15:17:45 +0300

    Working with assets

--- projects/project1/lib/gradient_container.dart
+++ projects/project1/lib/gradient_container.dart
@@ -19,7 +19,11 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(child: StyledText("Hello World!")),
+      child: Center(
+          child: Image.asset(
+        "assets/images/dice-1.png",
+        width: 200,
+      )),
     );
   }
 }
