84d598fb108e689a8c59ea66083626de6644d81a
Thunder <mahmmoud.hassanein@gmail.com>
2023-06-10 13:43:47 +0300

    Accepting passing functions as values.

--- practices/adv_basics/lib/start_screen.dart
+++ practices/adv_basics/lib/start_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
 
-  inPress() {}
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +37,7 @@ class StartScreen extends StatelessWidget {
             height: 30,
           ),
           OutlinedButton.icon(
-            onPressed: inPress,
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
