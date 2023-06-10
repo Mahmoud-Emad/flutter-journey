874ca5b5623bd022f319bc511c3a4a4e5ac8c387
Thunder <mahmmoud.hassanein@gmail.com>
2023-06-10 14:01:57 +0300

    Using ternary expressions & comparison operators

--- practices/adv_basics/lib/quiz.dart
+++ practices/adv_basics/lib/quiz.dart
@@ -12,17 +12,16 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
+  var activeScreen = 'start-screen';
 
   @override
   void initState() {
-    activeScreen = StartScreen(switchScreen);
     super.initState();
   }
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'quistions-screen';
     });
   }
 
@@ -41,7 +40,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
