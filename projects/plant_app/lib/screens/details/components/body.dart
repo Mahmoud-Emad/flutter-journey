74fa8cf59c57770943955fe02473e23a36dd2483
Thunder <mahmmoud.hassanein@gmail.com>
2023-06-21 23:03:57 +0300

    Move the title and price of the details screen to another component.

--- projects/plant_app/lib/screens/details/components/body.dart
+++ projects/plant_app/lib/screens/details/components/body.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:plant_app/constants.dart';
+// import 'package:plant_app/constants.dart';
 // import 'package:flutter_svg/svg.dart';
 
 import 'image_and_icons_card.dart';
+import 'title_and_price.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -14,43 +15,11 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ImageAndIconsCard(size: size),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: kDefualtPadding),
-            child: Row(
-              children: [
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "Mahmoud\n",
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineMedium
-                            ?.copyWith(
-                                color: kTextColor, fontWeight: FontWeight.bold),
-                      ),
-                      const TextSpan(
-                        text: "Egypt",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: kPrimaryColor,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                const Spacer(),
-                Text(
-                  "\$440",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineSmall
-                      ?.copyWith(color: kPrimaryColor),
-                ),
-              ],
-            ),
-          )
+          const TitleAndPrice(
+            title: "Mahmoud",
+            country: "Egypt",
+            price: 450,
+          ),
         ],
       ),
     );
