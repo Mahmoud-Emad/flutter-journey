788d7df91ba927c498a07fc7aba03b120291e626
Thunder <mahmmoud.hassanein@gmail.com>
2023-07-05 15:33:12 +0300

    Created a dummey data.

--- projects/expense_tracker/lib/expenses.dart
+++ projects/expense_tracker/lib/expenses.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/expense.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -10,12 +12,27 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registerdExpenses = [
+    Expense(
+      amount: 19.52,
+      title: "Flutter Course",
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      amount: 15.36,
+      title: "Cinema",
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
         children: [
-          Text("The chart"),
+          Text("The Chart"),
           Text("Expenses list..."),
         ],
       ),
